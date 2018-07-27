2548b9c33322af17cfb00aaeda9baf4289abc966
yifanren <15727798720@163.com>
2018-07-27 21:12:23 +0800

    7.27

--- subsection03/01_readFile.c
+++ subsection03/01_readFile.c
@@ -7,43 +7,43 @@
 #include <string.h>
 #include <strings.h>
 
-int num=0;
-off_t total=0;
-int fileCount=0;
+struct file{
+    int num;
+    off_t total;
+    int fileCount;
+};
 
-void IsFloder(char * folderPath);
+void countFileNum(char * folderPath,struct file *p_info);
 
 
 //give the parameter into function
 int main(int argc,char* argv[])
 {
     if(argc!=2){
-        printf("please input the folder path :");
+        printf("please input the folder path :\n");
+        exit(1);
     }
 
-    IsFloder(argv[1]);
+    struct file *p_info;   
+    p_info=(struct file *)malloc(sizeof(struct file));
 
-    printf("\n");
-    printf("^^^^^^^^^^^^^^^^^^^^^the main info^^^^^^^^^^^^^^^^^^^^^\n");
-    printf("the tatal Bytes is %d\n",total);
-    printf("there are %d file more than 1M\n",num);
-    printf("there are %d file\n",fileCount);
+    countFileNum(argv[1],p_info);
 
+    printf("the tatal Bytes is %ld\n",p_info->total);
+    printf("there are %d file more than 1M\n",p_info->num);
+    printf("there are %d file\n",p_info->fileCount);
+
+    free(p_info);
     return 0;
 }
 
-
-
-void IsFloder(char * folderPath)
+void countFileNum(char * folderPath,struct file  *p_info)
 {
     DIR *dp;
     struct dirent *entry;
     struct stat statbuf;
 
     char filename[128];
-
-    //    off_t total=0;
-    //    int num=0;
 
     //judge the path is file or folder
     //not folder
@@ -52,92 +52,30 @@ void IsFloder(char * folderPath)
         exit(1);    
     }
 
-    //is folder,enter the folder
-    //chdir(folderPath);
-
-
 
     //search all file in the folder
     while((entry=readdir(dp))!=NULL){
-        lstat(entry->d_name,&statbuf);
+            sprintf(filename,"%s/%s",folderPath,entry->d_name);
+        lstat(filename,&statbuf);
 
-        if(S_ISDIR(statbuf.st_mode)){
-            printf("-------------this wen jian jia --------%s\n",entry->d_name);
+//        if(S_ISDIR(statbuf.st_mode)){
+        if( DT_DIR & entry->d_type){
             if(strcmp(".",entry->d_name)==0||strcmp("..",entry->d_name)==0)
                 continue;
 
-            // strcat(filename,"/");
-            //strcat(filename,entry->d_name);
-            sprintf(filename,"%s/%s",folderPath,entry->d_name);
-
-            printf("***************%s*****************",filename);
-            IsFloder(filename);
+            countFileNum(filename,p_info);
 
         }else{
 
-            printf("-------------this is the file name--------%s\n",entry->d_name);
-
             if((statbuf.st_size)>(1024*1024))
-                num++;
+                p_info->num++;
 
-            total+=statbuf.st_size;
-            fileCount+=1;
+            p_info->total+=statbuf.st_size;
+            p_info->fileCount+=1;
         }
     }
 
-    //return the last item
-    //chdir("..");
 
     closedir(dp);
 
 }
-#if 0
-
-
-    if(entry->d_reclen==24){
-        if(entry->d_type==8){
-
-            printf("-------------this is the file name--------%s\n",entry->d_name);
-
-            if((statbuf.st_size)>(1024*1024))
-                num++;
-
-            total+=statbuf.st_size;
-            fileCount+=1;
-
-
-        }else{
-
-            printf("-------------this wen jian jia --------%s\n",entry->d_name);
-            if(strcmp(".",entry->d_name)==0||strcmp("..",entry->d_name)==0)
-                continue;
-
-            // strcat(filename,"/");
-            //strcat(filename,entry->d_name);
-            sprintf(filename,"%s/%s",folderPath,entry->d_name);
-
-            printf("***************%s*****************",filename);
-            IsFloder(filename);
-
-        }
-    }
-
-#endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
